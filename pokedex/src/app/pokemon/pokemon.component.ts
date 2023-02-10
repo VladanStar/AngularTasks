7ec23c53a8674cfa69d301ea298675226d7d807e
VladanStar <vladancupric@gmail.com>
2023-02-10 12:25:57 +0100

    Add

--- pokedex/src/app/pokemon/pokemon.component.ts
+++ pokedex/src/app/pokemon/pokemon.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input} from '@angular/core';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
-  styleUrls: ['./pokemon.component.css']
+  styleUrls: ['./pokemon.component.css'],
+// providers: [PokemonService];
 })
 export class PokemonComponent {
 @Input() pokemon:string ="";
