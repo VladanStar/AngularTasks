ad14d2628531b8d66620e9b3e5cf57b7e9aee5d8
VladanStar <vladancupric@gmail.com>
2023-02-11 19:59:53 +0100

    Add

--- shopAngularTask/src/app/services/product.service.ts
+++ shopAngularTask/src/app/services/product.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Product } from 'src/models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+private products:Product[];
 
-  constructor() { }
+  constructor() {
+this.products=[
+new Product(101, "Phone X",799,"A large phone"),
+new Product(102, "Phone Standard", 699,"A great phon"),
+new Product(103, "Phone mini",299,"")
+];
+  }
 }
