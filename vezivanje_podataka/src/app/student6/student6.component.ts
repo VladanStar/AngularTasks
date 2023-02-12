00b8aa93c960d28fc5a81096449a6eb40da7b17b
VladanStar <vladancupric@gmail.com>
2023-02-12 22:25:17 +0100

    Add

--- vezivanje_podataka/src/app/student6/student6.component.ts
+++ vezivanje_podataka/src/app/student6/student6.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./student6.component.css']
 })
 export class Student6Component {
+  static readonly listaImena=["Mateja","Shopi","Aleksandar","Dragan","Cedomir","Dejan"]
+  static readonly  listaPrezimena=["Popovic", "Sulejmanov","Rac","Vukasinovic","Tomcic","Kuzmanovic"]
+    ime:string ="";
+private menjanjeJeDozvoljeno:boolean=true;
+
+
+    constructor(){
+
+    }
+unosJeOnemogucen():boolean{
+return!this.menjanjeJeDozvoljeno;
+}
+
+onSacuvajInformacije():void{
+this.menjanjeJeDozvoljeno=false;
+}
 
 }
