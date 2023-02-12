c0d10191363feebe32e80507e1b047c9f7a3c6cc
VladanStar <vladancupric@gmail.com>
2023-02-12 20:06:53 +0100

    Add

--- vezivanje_podataka/src/app/student3/student3.component.ts
+++ vezivanje_podataka/src/app/student3/student3.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-student3',
   templateUrl: './student3.component.html',
   styleUrls: ['./student3.component.css'],
 })
-export class Student3Component implements OnInit {
+export class Student3Component  {
   static brojac: number = 1;
-  redniBroj: number =0;
-  jmbgStr: string = "";
+  redniBroj: number;
+  // jmbgStr: string = null;
 
   constructor() {
     this.redniBroj =Student3Component.brojac;
  ++Student3Component.brojac;
   }
-  ngOnInit(): void {}
 
-jmbg():string {
-if(this.jmbgStr === ""){
-this.jmbgStr =
-Array.apply( "")
-.map(val => Math.floor(Math.random() *10))
-.join('');
-}
-return this.jmbgStr;
 
+// jmbg(): string {
+// if(this.jmbgStr === null){
+// this.jmbgStr =
+// Array.apply( null,{length:13})
+// .map(val => Math.floor(Math.random() *10))
+// .join('');
+// }
+// console.log(this.jmbgStr)
+// return this.jmbgStr;
+//}
 }
-}
+
