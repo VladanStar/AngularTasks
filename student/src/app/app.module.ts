30953a04f0b117f4e75278691c3af74fec8d45b7
VladanStar <vladancupric@gmail.com>
2023-02-16 16:04:02 +0100

    Add

--- student/src/app/app.module.ts
+++ student/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Student1Component } from './student1/student1.component';
 import { Student1ListComponent } from './student1-list/student1-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Student1Component,
-    Student1ListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, Student1Component, Student1ListComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
